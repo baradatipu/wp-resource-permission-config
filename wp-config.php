4cdda825722650dd3553d1a857d4a6a8769b333f
Barada Prasanna Pradhan <baradaprasanna03@gmail.com>
2024-04-30 17:56:51 +0530

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,9 @@
+@ini_set( ‘upload_max_filesize’ , ‘256M’ );
+
+@ini_set( ‘post_max_size’, ‘256M’);
+
+@ini_set( ‘memory_limit’, ‘256M’ );
+
+@ini_set( ‘max_execution_time’, ‘300’ );
+
+@ini_set( ‘max_input_time’, ‘300’ );
